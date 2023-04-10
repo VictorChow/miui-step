9686369bf2a23c5ebd4a02241873f185f6a09c4a
Victor Chow <285050252@qq.com>
2023-04-10 14:59:50 +0800

    添加减少步数

--- app/src/main/java/com/victor/miuistep/MainActivity.kt
+++ app/src/main/java/com/victor/miuistep/MainActivity.kt
@@ -2,11 +2,13 @@ package com.victor.miuistep
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import com.jaredrummler.android.shell.Shell
 import com.victor.miuistep.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private val shell =
         "content insert --uri content://com.miui.providers.steps/item --bind _begin_time:s:`date +%s`000 --bind _id:i:`echo ${'$'}RANDOM` --bind _end_time:s:`date +%s`999 --bind _mode:i:2 --bind _steps:i:"
@@ -16,10 +18,24 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnStep.setOnClickListener {
-            val num = binding.etStep.text.toString().toIntOrNull() ?: return@setOnClickListener
-            Shell.SU.run(shell + num)
-            Toast.makeText(this, "操作成功", Toast.LENGTH_SHORT).show()
-        }
+        binding.btnStepAdd.setOnClickListener(this)
+        binding.btnStepSub.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        binding.btnStepAdd.isEnabled = false
+        binding.btnStepSub.isEnabled = false
+
+        var num = binding.etStep.text.toString().toIntOrNull() ?: return
+        if (v == binding.btnStepSub) num = -num
+        val result = Shell.SU.run(shell + num)
+        Toast.makeText(
+            this,
+            if (result.isSuccessful) "操作成功" else result.getStderr(),
+            Toast.LENGTH_SHORT
+        ).show()
+
+        binding.btnStepAdd.isEnabled = true
+        binding.btnStepSub.isEnabled = true
     }
 }
